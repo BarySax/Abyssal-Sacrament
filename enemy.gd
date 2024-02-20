bb6531e0a9a21f6487eb4183a115997632c4239c
Gabriel Foriel-Fusier <forielfusierg@educ.college-montreal.qc.ca>
2024-02-20 09:29:58 -0500

    enemy little better

--- enemy.gd
+++ enemy.gd
@@ -4,6 +4,7 @@ extends Area2D
 @export var see_range = 300
 var player_position
 var target_position
+var player_seen = false
 @onready var player = get_parent().get_node("Player")
 
 # Called when the node enters the scene tree for the first time.
@@ -16,9 +17,11 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	player_position = player.position
 	target_position = (player_position - position).normalized()
-	
-	if position.distance_to(player_position) < attack_range or position.distance_to(player_position) > see_range:
-		velocity = 0
-	else:
-		velocity = target_position * speed
-		position += velocity * delta
+	if position.distance_to(player_position) < see_range:
+		player_seen = true
+	if player_seen == true:
+		if position.distance_to(player_position) < attack_range:
+			velocity = 0
+		else:
+			velocity = target_position * speed
+			position += velocity * delta
