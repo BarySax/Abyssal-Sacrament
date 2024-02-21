4b39b3c08a4a16d2ba096f6a0f50f1ff6ebc1f17
BarySax <gabriel.forielfusier@icloud.com>
2024-02-20 21:12:44 -0500

    Advancing

---
+++ attack_rotation.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	look_at(get_global_mouse_position())
