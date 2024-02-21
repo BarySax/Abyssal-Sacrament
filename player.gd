1327f1f2032f35215c5720f4a32732b696c4529c
Gabriel Foriel-Fusier <forielfusierg@educ.college-montreal.qc.ca>
2024-02-21 13:21:17 -0500

    IWANNAKIllMYSELF

--- player.gd
+++ player.gd
@@ -13,6 +13,7 @@ var speed = walking_speed
 var running = false
 var turning = false
 var attack_rotation
+var attack_direction = "Right"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -58,7 +59,40 @@ func _process(delta):
 			else:
 				turning = false
 				print("not turning")
-		if turning == true:
+		
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite2D.play()
+		else:
+			$AnimatedSprite2D.stop()
+
+		
+		position += velocity * delta
+		if turning == false:
+			if velocity.y < 0 and velocity.x != 0:
+				$AnimatedSprite2D.animation = "walk_down_left"
+				$AnimatedSprite2D.flip_v = false
+				# See the note below about boolean assignment.
+				$AnimatedSprite2D.flip_h = velocity.x > 0
+			elif velocity.y > 0 and velocity.x != 0:
+				$AnimatedSprite2D.animation = "walk_up_left"
+				$AnimatedSprite2D.flip_v = false
+				# See the note below about boolean assignment.
+				$AnimatedSprite2D.flip_h = velocity.x > 0	
+			elif velocity.y > 0:
+				$AnimatedSprite2D.animation = "walk_down"
+			elif velocity.x != 0:
+				$AnimatedSprite2D.animation = "walk_left"
+				$AnimatedSprite2D.flip_v = false
+				# See the note below about boolean assignment.
+				$AnimatedSprite2D.flip_h = velocity.x > 0		
+			elif velocity.y < 0:
+				$AnimatedSprite2D.animation = "walk_up"
+				#$AnimatedSprite2D.flip_v = velocity.y > 0
+			elif velocity.y > 0:
+				$AnimatedSprite2D.animation = "walk_down"
+		
+		else:
 			attack_rotation = get_node("Attack_rotation").get_rotation()
 			attack_rotation = rad_to_deg(attack_rotation)
 			if attack_rotation <= -360:
@@ -71,96 +105,82 @@ func _process(delta):
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 1
+				attack_direction = "Right"
 			elif attack_rotation <= 67 and attack_rotation >= 23:
 				$AnimatedSprite2D.animation = "walk_down_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 1
+				attack_direction = "Down_Right"
 			elif attack_rotation <= 112  and attack_rotation >= 68:
 				$AnimatedSprite2D.animation = "walk_down"
+				attack_direction = "Down"
 			elif attack_rotation <= 157  and attack_rotation >= 113:
 				$AnimatedSprite2D.animation = "walk_down_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 0
+				attack_direction = "Down_Left"
 			elif attack_rotation <= 202  and attack_rotation >= 158:
 				$AnimatedSprite2D.animation = "walk_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 0
+				attack_direction = "Left"
 			elif attack_rotation <= 247  and attack_rotation >= 203:
 				$AnimatedSprite2D.animation = "walk_up_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 0
+				attack_direction = "Up_Left"
 			elif attack_rotation <= 292  and attack_rotation >= 248:
 				$AnimatedSprite2D.animation = "walk_up"
+				attack_direction = "Up"
 			elif attack_rotation <= 337  and attack_rotation >= 293:
 				$AnimatedSprite2D.animation = "walk_up_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 1
+				attack_direction = "Up_Right"
 			elif attack_rotation >= -67 and attack_rotation <= -23:
 				$AnimatedSprite2D.animation = "walk_up_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 1
+				attack_direction = "Up_Right"
 			elif attack_rotation >= -112  and attack_rotation <= -68:
 				$AnimatedSprite2D.animation = "walk_up"
+				attack_direction = "Up"
 			elif attack_rotation >= -157  and attack_rotation <= -113:
 				$AnimatedSprite2D.animation = "walk_up_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 0
+				attack_direction = "Up_Left"
 			elif attack_rotation >= -202  and attack_rotation <= -158:
 				$AnimatedSprite2D.animation = "walk_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 0
+				attack_direction = "Left"
 			elif attack_rotation >= -247  and attack_rotation <= -203:
 				$AnimatedSprite2D.animation = "walk_down_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 0
+				attack_direction = "Down_Left"
 			elif attack_rotation >= -292  and attack_rotation <= -248:
 				$AnimatedSprite2D.animation = "walk_down"
+				attack_direction = "Down"
 			elif attack_rotation >= -337  and attack_rotation <= -293:
 				$AnimatedSprite2D.animation = "walk_down_left"
 				$AnimatedSprite2D.flip_v = false
 				# See the note below about boolean assignment.
 				$AnimatedSprite2D.flip_h = 1
-			print(attack_rotation)
-		if velocity.length() > 0:
-			velocity = velocity.normalized() * speed
-			$AnimatedSprite2D.play()
-		else:
-			$AnimatedSprite2D.stop()
-
-		
-		position += velocity * delta
-		if velocity.y < 0 and velocity.x != 0:
-			$AnimatedSprite2D.animation = "walk_down_left"
-			$AnimatedSprite2D.flip_v = false
-			# See the note below about boolean assignment.
-			$AnimatedSprite2D.flip_h = velocity.x > 0
-		elif velocity.y > 0 and velocity.x != 0:
-			$AnimatedSprite2D.animation = "walk_up_left"
-			$AnimatedSprite2D.flip_v = false
-			# See the note below about boolean assignment.
-			$AnimatedSprite2D.flip_h = velocity.x > 0	
-		elif velocity.y > 0:
-			$AnimatedSprite2D.animation = "walk_down"
-		elif velocity.x != 0:
-			$AnimatedSprite2D.animation = "walk_left"
-			$AnimatedSprite2D.flip_v = false
-			# See the note below about boolean assignment.
-			$AnimatedSprite2D.flip_h = velocity.x > 0		
-		elif velocity.y < 0:
-			$AnimatedSprite2D.animation = "walk_up"
-			#$AnimatedSprite2D.flip_v = velocity.y > 0
-		elif velocity.y > 0:
-			$AnimatedSprite2D.animation = "walk_down"
-		
-		
+				attack_direction = "Down_Right"
+			
+			if Input.is_action_just_pressed("attack"):
+					print(attack_direction)
+					
 	else:
 		print("Dead")
